e2f7d4503bdf6ce8ee8d2f79bb22a082c803cb2b
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-03-26 13:12:57 +0700

    Add files via upload

---
+++ 16/outputText2.m
@@ -0,0 +1,17 @@
+clear
+clc
+
+% Import data dengan delimeter dan Header
+filename = 'data2.txt';
+delimeter = ' ';    % <-- Perhatikan data jika memiliki koma (,) maka delimeter harus memiliki koma begitu juga dengan titik koma (;) 
+headers = 1;
+
+Data = importdata(filename,delimeter,headers);
+
+waktu = Data.data(:,1);
+suhu = Data.data(:,2);
+
+figure(2)
+plot(waktu,suhu)
+xlabel(Data.colheaders{1,1});
+ylabel(Data.colheaders{1,2});
