69c8eb46be4b6a6c6ae28559805db6c6eecd37d3
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-03-26 13:11:39 +0700

    Add files via upload

---
+++ 13/data.m
@@ -0,0 +1,5 @@
+clear
+clc
+
+data = csvread('data.csv');   % read data on file <-- SUCCESS
+data = xlsread('data.xls');   % read data on file 
---
+++ 13/numTxtRaw.m
@@ -0,0 +1,8 @@
+clear
+clc
+
+[num, txt, raw] = xlsread('data.xlsx','Sheet1','A1:B10');
+
+% Num -- Numeric
+% Txt -- Text
+% Raw -- Ambil Seluruhnya
