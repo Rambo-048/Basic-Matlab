be8fe391d59f615914975c1b534a980a65ce2f40
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-03-28 00:14:30 +0700

    Add files via upload

---
+++ 21/for.m
@@ -0,0 +1,12 @@
+clear
+clc
+
+% A = 20;
+
+for A = 1:20
+teks_format = sprintf('Nilai A = %d\n',A);
+fprintf(teks_format);
+end
+
+% sprintf ==> print untuk formating teks/ teks format
+% fprintf ==> Langsung munculkan Nilai A = 20
