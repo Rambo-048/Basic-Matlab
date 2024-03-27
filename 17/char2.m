22c8bcc617c01107c3b1282754dd3333570564c6
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-03-27 08:27:18 +0700

    Add files via upload

---
+++ 17/char2.m
@@ -0,0 +1,38 @@
+clear
+clc
+
+% Membuat String
+huruf1 = 'abcdefghijklmnopqrstuvwxyz';
+huruf2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+% Indexing di Char
+b = huruf1(2);
+o = huruf1(5);
+a_f = huruf1(1:15);
+
+% Mapping
+b_ascii = uint8(b);   % uint8(unsigned 8 bit)
+f_ascii = uint8(f);
+
+huruf_ascii = uint8(huruf1)
+
+a = char(97);
+c = char(99);
+d = char(100);
+
+% Append
+nama1 = 'Beck';
+nama1(6) = 'z';
+nama1(7) = 'a';
+
+nama2 = ' Cuthbert';
+
+% concat
+nama_saya = [nama1 nama2];
+
+nama3 = 'Kong';
+nama4 = 'Guan';
+nama5 = 'Asuka';
+nama6 = 'Sakiri';
+
+nama = {nama1, nama2, nama3, nama4, nama5, nama6}
