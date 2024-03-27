4aaad4ddc8d0b3cc95beae52826c83e666383026
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-03-27 08:27:33 +0700

    Add files via upload

---
+++ 18/char1.m
@@ -0,0 +1,36 @@
+clear
+clc
+
+% Membuat String
+huruf1 = 'abcdefghijklmnopqrstuvwxyz';
+huruf2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+% Indexing di Char
+b = huruf1(2);
+o = huruf1(5);
+a_f = huruf1(1:15);
+
+% Mapping
+b_ascii = uint8(b);   % uint8(unsigned 8 bit)
+f_ascii = uint8(f);
+
+huruf_ascii = uint8(huruf1)
+
+a = char(111);
+c = char(116);
+d = char(100);
+
+% Append
+nama1 = 'Eli';
+nama1(4) = 'z';
+nama1(5) = 'a';
+
+nama2 = ' Cuthbert';
+
+% concat
+nama_saya = [nama1 nama2];
+
+nama3 = 'Kripik';
+nama4 = 'Roshan';
+
+nama = {nama1, nama2, nama3, nama4}
---
+++ 18/char_analysis1.m
@@ -0,0 +1,19 @@
+clear
+clc
+
+data = 's0mpl4k 5uMp4h!&';
+
+% Fungsi built_in matlab
+% Menganalisa data char
+
+a = ischar(data); % ischar berfungsi mengecek bahwa ini adalah data char atau bukan == 1 = True, 0 = False
+b = isletter(data);  % Ambil huruf
+c = isspace(data);   % Ambil Spasi
+
+d = isstrprop(data,'alpha');    % Ambil huruf saja == isletter
+e = isstrprop(data,'alphanum'); % 'alphanum' Ambil Angka dan Huruf kecuali Symbol
+f = isstrprop(data, 'digit');   % Ambil Angka
+g = isstrprop(data, 'lower');   % Ambil Huruf Kecil
+h = isstrprop(data,'upper');    % Ambil Huruf Besar
+i = isstrprop(data,'punct');    % Ambil Tanda Baca
+j = isstrprop(data,'wspace');   % Ambil Spasi
