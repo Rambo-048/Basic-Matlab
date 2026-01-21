91b213a1066bf61e655aa5d3529f045121548c32
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-21 22:00:27 +0700

    Update dataStruct4.m

--- 30/dataStruct4.m
+++ 30/dataStruct4.m
@@ -6,16 +6,16 @@ Costumer = struct();
 Costumer(1).id_number = 72658775;
 Costumer(1).data_yang_dibutuhkan__1 = 'data_C';
 Costumer(1).jumlah_yang_dibutuhkan__1 = 10;
-Costumer(1).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 1112;
-%Costumer(1).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = ;
+Costumer(1).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 1112;
+%Costumer(1).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = ;
 
 
 
 Costumer(2).id_number = 71827377;
 Costumer(2).data_yang_dibutuhkan__1 = 'data_k';
 Costumer(2).jumlah_yang_dibutuhkan__1 = 10;                                         % Program akan otomatis membuat data kosong jika tidak ada field tertentu pada data
-Costumer(2).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 2140;            % Struct didalam struct
-%Costumer(2).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = ; 
+Costumer(2).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 2140;            % Struct didalam struct
+%Costumer(2).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = ; 
 
 
 
@@ -24,8 +24,8 @@ Costumer(3).data_yang_dibutuhkan__1 = 'data_c';
 Costumer(3).data_yang_dibutuhkan__2 = 'data_k';
 Costumer(3).jumlah_yang_dibutuhkan__1 = 5;
 Costumer(3).jumlah_yang_dibutuhkan__2 = 10;
-Costumer(3).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 4022;            % Struct didalam struct
-Costumer(3).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = 2140;
+Costumer(3).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 4022;            % Struct didalam struct
+Costumer(3).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = 2140;
 
 
 
@@ -35,6 +35,6 @@ Costumer(4).data_yang_dibutuhkan__1 = 'data_K';
 Costumer(4).data_yang_dibutuhkan__2 = 'data_C';
 Costumer(4).jumlah_yang_dibutuhkan__1 = 15;
 Costumer(4).jumlah_yang_dibutuhkan__2 = 10;
-Costumer(4).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 3279;             % Struct didalam struct
-Costumer(4).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = 1112;   
+Costumer(4).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 3279;             % Struct didalam struct
+Costumer(4).address.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = 1112;   
 
