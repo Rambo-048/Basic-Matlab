20e2aa85375380bb2fedf9957f4dba467fd40324
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-21 21:29:09 +0700

    Add files via upload

---
+++ 30/dataStruct4.m
@@ -0,0 +1,40 @@
+clear
+clc
+
+Costumer = struct();
+
+Costumer(1).id_number = 72658775;
+Costumer(1).data_yang_dibutuhkan__1 = 'data_C';
+Costumer(1).jumlah_yang_dibutuhkan__1 = 10;
+Costumer(1).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 1112;
+%Costumer(1).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = ;
+
+
+
+Costumer(2).id_number = 71827377;
+Costumer(2).data_yang_dibutuhkan__1 = 'data_k';
+Costumer(2).jumlah_yang_dibutuhkan__1 = 10;                                         % Program akan otomatis membuat data kosong jika tidak ada field tertentu pada data
+Costumer(2).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 2140;            % Struct didalam struct
+%Costumer(2).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = ; 
+
+
+
+Costumer(3).id_number = 7065738289;
+Costumer(3).data_yang_dibutuhkan__1 = 'data_c';
+Costumer(3).data_yang_dibutuhkan__2 = 'data_k';
+Costumer(3).jumlah_yang_dibutuhkan__1 = 5;
+Costumer(3).jumlah_yang_dibutuhkan__2 = 10;
+Costumer(3).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 4022;            % Struct didalam struct
+Costumer(3).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = 2140;
+
+
+
+
+Costumer(4).id_number = 7079826769;
+Costumer(4).data_yang_dibutuhkan__1 = 'data_K';
+Costumer(4).data_yang_dibutuhkan__2 = 'data_C';
+Costumer(4).jumlah_yang_dibutuhkan__1 = 15;
+Costumer(4).jumlah_yang_dibutuhkan__2 = 10;
+Costumer(4).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__1 = 3279;             % Struct didalam struct
+Costumer(4).value.berdasarkan_jumlah_dan_data_yang_dibutuhkan__2 = 1112;   
+
