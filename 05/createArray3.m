497e5910619e433631e3609c28c2157d51e4608e
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-22 20:33:51 +0700

    Add files via upload

---
+++ 05/createArray3.m
@@ -0,0 +1,9 @@
+clear
+clc
+
+% Membuat Array dengan Increment
+a = 1 : 10  % artinya a isinya 1 2 3 4 5 6 7 8 9 10
+
+% dengan perubahan Stepnya
+b = 1 : 1.5 : 10 % atau 
+c = 1 :  .5 : 10 % Sama Saja
