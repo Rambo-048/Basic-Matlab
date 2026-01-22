274e5ca3819d58821106e5db791b3f53a9b049ce
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-22 19:57:34 +0700

    Update Matrix1.m

--- 03/Matrix1.m
+++ 03/Matrix1.m
@@ -2,7 +2,7 @@ clear
 clc
 
 % Skalar
-a = 7;
+a = 10;
 
 % Vector
 b = [ 1 5 7 9 3 ]; %Vector Kolom
@@ -23,8 +23,8 @@ sum = b + c';
 mul = dot(b,c');
 
 % Perkalian Vector Cross -- Hanya Bisa terdiri dari 3 dimensi karena a dan b 5 dimensi maka dibuat e dan f yang masing2 berisi 3 dimensi
-e = [ 1 3 4 ];
-f = [ 6 7 8 ];
+e = [ 3 5 7 ];
+f = [ 4 6 8 ];
 
 mulyanto = cross(e,f);
 
@@ -34,4 +34,5 @@ h = [b; c'];
 
 % i = [b;e] <-- Jika Seperti ini maka eror dikarenakan e hanya terdiri dari 3 dimensi sedangkan b 5 dimensi, untuk mengakalinya maka :
   
-  i = [b;e 7 8];
+  i = [b;e 9 1];
+
