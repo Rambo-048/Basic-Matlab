3f78caaa62c60af291e00cb8ecbb705da49f6018
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-22 20:45:26 +0700

    Delete 05/createArray3.m

--- 05/createArray3.m
+++
@@ -1,9 +0,0 @@
-clear
-clc
-
-% Membuat Array dengan Increment
-a = 1 : 10  % artinya a isinya 1 2 3 4 5 6 7 8 9 10
-
-% dengan perubahan Stepnya
-b = 1 : 1.5 : 10 % atau 
-c = 1 :  .5 : 10 % Sama Saja
