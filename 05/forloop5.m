fc09b650411fbad315a1bd3dc17fa227be1686a2
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-22 20:49:37 +0700

    Add files via upload

---
+++ 05/forloop5.m
@@ -0,0 +1,24 @@
+clear
+clc
+
+% Membuat Array dengan Increment
+a = 0 : 10  % artinya a isinya 0 1 2 3 4 5 6 7 8 9 10
+
+% dengan perubahan Stepnya
+b = 0 : 0.5 : 10 % atau 
+c = 0 :  .5 : 10 % Sama Saja
+
+% Contoh Soal
+% Misalkan y = x^2 + 2x
+% Misalkan x = -10;10 
+
+% For loop untuk contoh soal diatas
+
+x = -10:10;
+y = [];
+
+for i = 1 : length(x)
+  y(i) = x(i)^2 + 2*x(i);
+end
+
+plot (x,y)
