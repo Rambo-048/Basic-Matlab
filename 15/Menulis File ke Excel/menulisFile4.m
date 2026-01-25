1ded88861050156a3aea57b8a22d575a575ce2a9
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-25 12:52:15 +0700

    Add files via upload

---
+++ 15/Menulis File ke Excel/menulisFile4.m
@@ -0,0 +1,22 @@
+clear
+clc
+
+%% Siapkan Data Untuk Ditulis di Excel
+x = 1:30;
+y = x.^5;
+Data[x' y'];
+DataCell = num2cell(data); % <-- samakan data
+Header = {'angka','kuadrat'};
+% [Header;Data] penulisan langsung seperti ini tidak bisa karena perbedaan Length maka Data Perlu kita samakan terlebih dahulu
+
+%% Menulis Ke Excel
+filename = 'datakuadrat.xls'; % jika ingin mengganti format langsung ubah disini .xlsx lalu run
+A = [Header;DataCell];
+sheet = 'kuadrat';
+xlrange = 'dl'
+xlswrite(filename,A,sheet,xlrange)
+
+% CATATAN : Menulis data ke Windows itu bisa dikarenakan di Windows ada Excel Server
+%           Tapi di Mac atau Linux tidak bisa karena tidak memiliki Excel Server karena akan Otomatis berubah menjadi CSV
+% Data = {'time',1;2;3}  % <-- Penulisan Data Cell
+
