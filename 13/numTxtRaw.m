48c011b70912387f31f931e8cfb906352d078862
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-25 11:50:44 +0700

    Update numTxtRaw.m

--- 13/numTxtRaw.m
+++ 13/numTxtRaw.m
@@ -1,8 +1,9 @@
 clear
 clc
 
-[num, txt, raw] = xlsread('data.xlsx','Sheet1','A1:B10');
+[num, txt, raw] = xlsread('data.xlsx','Sheet1','A1:B11');
 
 % Num -- Numeric
 % Txt -- Text
 % Raw -- Ambil Seluruhnya
+
