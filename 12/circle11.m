b016bcd7146524ab84eb9f7aac6e96e3f2d547f9
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-26 09:25:12 +0700

    Add files via upload

---
+++ 12/circle11.m
@@ -0,0 +1,27 @@
+clear
+clc
+
+jarijari = 40;
+X0 = 0;
+Y0 = 0;
+
+% RUMUS -- hasil = lingkaran(r,posx,posy) 
+
+[X1,Y1] = lingkaran(jarijari,X0,Y0);
+[X2,Y2] = lingkaran(5,-10,-5);
+[X3,Y3] = lingkaran(5,-15,10);
+[X4,Y4] = lingkaran(5,-20,-15);
+[X5,Y5] = lingkaran(5,-25,20);
+[X6,Y6] = lingkaran(5,-30,-25);
+[X7,Y7] = lingkaran(5,-35,-30);
+
+
+plot(X1,Y1)
+hold on
+plot(X2,Y2)
+plot(X3,Y3)
+plot(X4,Y4)
+plot(X5,Y5)
+plot(X6,Y6)
+plot(X7,Y7)
+hold off
