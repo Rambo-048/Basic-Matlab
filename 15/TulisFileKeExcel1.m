8e16e1ee9dade49a4a45ca429666b7f0bb556325
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-26 10:54:43 +0700

    Update TulisFileKeExcel1.m

--- 15/TulisFileKeExcel1.m
+++ 15/TulisFileKeExcel1.m
@@ -3,7 +3,7 @@ clc
 
 x = 1:10;
 y = x.^2;
-Data[x' y'];
+Data = [x' y'];
 
 % xlswrite('data.xls',Data,'kuadrat','B2');  <-- Tidak bisa menggunkan ini disini, Gunakan csvwrite
 filename = 'data.csv'
