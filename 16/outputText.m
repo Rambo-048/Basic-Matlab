b7a76503de448ab50f49b2b1b328abddbdf02820
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-26 19:33:27 +0700

    Update outputText.m

--- 16/outputText.m
+++ 16/outputText.m
@@ -2,7 +2,7 @@ clear
 clc
 
 % Import Data Text
-filename1 = 'data1.txt';
+filename1 = 'Data1/data1a.txt';
 A = importdata(filename1);
 % A.^2 % dipangkatkan 2
 
@@ -13,18 +13,19 @@ figure(1);
 image(B);
 
 % Import Data delimiter
-filename3 = 'data1.txt';
+filename3 = 'Data1/data1a.txt';
 delimeter3 = ' ';
 C1 = importdata(filename3,delimeter3);
 
-filename4 = 'data1b.txt';
+filename4 = 'Data1/data1b.txt';
 delimeter4 = ' ';
 C2 = importdata(filename4,delimeter4);
 
-filename5 = 'data1c.txt';
+filename5 = 'Data1/data1c.txt';
 delimeter5 = ';';
 C3 = importdata(filename5,delimeter5);
 
-filename6 = 'data1c.txt';
+filename6 = 'Data1/data1c.txt';
 delimeter6 = ' ';
 C4 = importdata(filename6,delimeter6);
+
