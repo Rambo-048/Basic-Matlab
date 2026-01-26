0d349a09e58b34763d60e2f85c7f1d13647c5b70
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-26 19:33:43 +0700

    Update outputText2.m

--- 16/outputText2.m
+++ 16/outputText2.m
@@ -2,7 +2,7 @@ clear
 clc
 
 % Import data dengan delimeter dan Header
-filename = 'data2.txt';
+filename = 'Data1/data2.txt';
 delimeter = ' ';    % <-- Perhatikan data jika memiliki koma (,) maka delimeter harus memiliki koma begitu juga dengan titik koma (;) 
 headers = 1;
 
@@ -14,4 +14,5 @@ suhu = Data.data(:,2);
 figure(2)
 plot(waktu,suhu)
 xlabel(Data.colheaders{1,1});
+
 ylabel(Data.colheaders{1,2});
