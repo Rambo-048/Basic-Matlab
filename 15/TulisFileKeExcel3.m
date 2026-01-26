996ae0a082341b63f0bbaf7afdbd61b489c25424
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-26 19:12:17 +0700

    Add files via upload

---
+++ 15/TulisFileKeExcel3.m
@@ -0,0 +1,14 @@
+clear
+clc
+
+x = 1:30;
+y = x.^2;
+Data = [x' y'];
+
+% xlswrite('data.xls',Data,'kuadrat','B2');  <-- Tidak bisa menggunkan ini disini, Gunakan csvwrite
+filename = 'data.csv'
+X = filename;
+row = 0;
+col = 0;
+
+csvwrite(X,row,col);
