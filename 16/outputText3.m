5318bdc2bfdb04c1deda4976ce97ca90f664e660
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-26 20:00:34 +0700

    Add files via upload

---
+++ 16/outputText3.m
@@ -0,0 +1,18 @@
+clear
+clc
+
+% Import Data Text
+filename1 = 'Data2/data1.txt';
+filename2 = 'Data2/data2.txt';
+filename3 = 'Data2/data3.txt';
+filename4 = 'Data2/data4.txt';
+filename5 = 'Data2/data5.txt';
+
+A = importdata(filename1);
+B = importdata(filename2);
+C = importdata(filename3);
+D = importdata(filename4);
+E = importdata(filename5);
+
+% A.^2 % dipangkatkan 2
+
