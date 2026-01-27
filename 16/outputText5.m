9103374c5193a1b25c2e8bf0e7379cc8eaa2a0af
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-27 13:29:18 +0700

    Add files via upload

---
+++ 16/outputText5.m
@@ -0,0 +1,22 @@
+clear
+clc
+
+filename1 = 'Data3/data1.txt';
+delimeter1 = ';';
+data1 = importdata(filename1,delimeter1);
+
+filename2 = 'Data3/data2.txt';
+delimeter2 = ' ';
+data2 = importdata(filename2,delimeter2);
+
+filename3 = 'Data3/data3.txt';
+delimeter3 = ';';
+data3 = importdata(filename3,delimeter3);
+
+filename4 = 'Data3/data4.txt';
+delimeter4 = ' ';
+data4 = importdata(filename4,delimeter4);
+
+filename5 = 'Data3/data5.txt';
+delimeter5 = ',';
+data5 = importdata(filename5,delimeter5);
