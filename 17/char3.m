238d7ce152bae55a8189f14280fa2d0e3cc0be7b
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-27 20:43:16 +0700

    Add files via upload

---
+++ 17/char3.m
@@ -0,0 +1,42 @@
+clear
+clc
+
+% Membuat String
+huruf1 = 'abcdefghijklmnopqrstuvwxyz';
+huruf2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+% Indexing di Char
+data1 = huruf1(6);
+data2 = huruf1(10);
+data3 = huruf2(22);
+data4 = huruf2(26);
+data5 = huruf1(1:20);
+
+% Mapping
+a_ascii = uint8(data1);   % uint8(unsigned 8 bit)
+b_ascii = uint8(data2);
+
+ascii_number = uint8(huruf1)
+
+%A = char(48);
+%B = char(49);
+%C = char(50);
+%D = char(51);
+%E = char(52);
+
+% Append 1
+menu1 = 'Sand';
+menu1(5) = 'w';
+menu1(6) = 'i';
+menu1(7) = 'c';
+menu1(8) = 'h';
+
+menu2 = 'Alpukat';
+Menu_Sarapan = [menu1 menu2];
+
+
+% Append 2
+menu3 = 'Salad';
+menu4 = 'Martabak';
+
+Menu = {menu1, menu2, menu3, menu4}
---
+++ 17/char4.m
@@ -0,0 +1,49 @@
+clear
+clc
+
+% Membuat String
+huruf1 = 'abcdefghijklmnopqrstuvwxyz';
+huruf2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+% Indexing di Char
+data1 = huruf1(19);
+data2 = huruf2(26);
+data3 = huruf1(14);
+data4 = huruf2(22);
+data5 = huruf1(1:20);
+
+% Mapping
+a_ascii = uint8(data1);   % uint8(unsigned 8 bit)
+b_ascii = uint8(data2);
+
+ascii_number = uint8(huruf1)
+
+%A = char(48);
+%B = char(49);
+%C = char(50);
+%B = char(51);
+%C = char(52);
+
+% Append
+nama1 = 'Pan';
+nama1(4) = 'c';
+nama1(5) = 'a';
+menu1(6) = 'k';
+menu1(7) = 'e';
+
+nama2 = 'Mayonaise';
+
+% concat
+Menu_Sarapan = [menu1 menu2];
+
+menu3 = 'Sandwich';
+menu4 = 'Martabak';
+menu5 = 'Salad';
+menu6 = 'Pizza';
+menu7 = 'Nugget Rica-Rica';
+menu8 = 'Bolu Kismis';
+menu9 = 'Burger';
+menu10 = 'Taco';
+
+Menu = {menu1, menu2, menu3, menu4, menu5, menu6, menu7, menu8, menu9, menu10}
+
