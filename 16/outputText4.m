b5e6716f61e27f8124bafed1b664a14a93ab7d4a
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-27 12:46:29 +0700

    Add files via upload

---
+++ 16/outputText4.m
@@ -0,0 +1,47 @@
+clear
+clc
+
+filename1 = 'Gambar/pic1.png';
+A = imread(filename1);
+figure(1);
+image(A);
+
+filename2 = 'Gambar/pic2.png';
+B = imread(filename2);
+figure(2);
+image(B);
+
+filename3 = 'Gambar/pic3.png';
+C = imread(filename3);
+figure(3);
+image(C);
+
+filename4 = 'Gambar/pic4.png';
+D = imread(filename4);
+figure(4);
+image(D);
+
+filename5 = 'Gambar/pic5.png';
+E = imread(filename5);
+figure(5);
+image(E);
+
+filename6 = 'Gambar/pic6.png';
+F = imread(filename6);
+figure(6);
+image(F);
+
+filename7 = 'GambarIMG/pic7.png';
+G = imread(filename7);
+figure(7);
+image(G);
+
+filename8 = 'Gambar/pic8.png';
+H = imread(filename8);
+figure(8);
+image(H);
+
+filename9 = 'Gambar/pic9.png';
+I = imread(filename9);
+figure(9);
+image(I);
