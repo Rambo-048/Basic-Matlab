df750ff84ae1dd76f773089e28146b885aadea6e
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-28 13:10:52 +0700

    Add files via upload

---
+++ 18/char_analysis7.m
@@ -0,0 +1,19 @@
+clear
+clc
+
+DATA = 'GuL4! cUm1 s4mB4L k3C4p';
+
+% Fungsi built_in matlab
+% Menganalisa data char
+
+data_char = ischar(DATA);       % ischar berfungsi mengecek bahwa ini adalah data char atau bukan == 1 = True, 0 = False
+data_letter = isletter(DATA);   % Ambil huruf
+data_space = isspace(DATA);     % Ambil Spasi
+
+data_string1 = isstrprop(DATA,'alpha');    % Ambil huruf saja == isletter
+data_string2 = isstrprop(DATA,'alphanum'); % 'alphanum' Ambil Angka dan Huruf kecuali Symbol
+data_string3 = isstrprop(DATA,'digit');    % Ambil Angka
+data_string4 = isstrprop(DATA,'lower');    % Ambil Huruf Kecil
+data_string5 = isstrprop(DATA,'upper');    % Ambil Huruf Besar
+data_string6 = isstrprop(DATA,'punct');    % Ambil Tanda Baca
+data_string7 = isstrprop(DATA,'wspace');   % Ambil Spasi
