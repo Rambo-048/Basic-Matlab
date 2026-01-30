999568d10267611c58d0f902d9ed165e4f442749
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-30 12:03:07 +0700

    Add files via upload

--- 20/formatstring3.m
+++ 20/formatstring3.m
@@ -1,7 +1,7 @@
 clear
 clc
 
-VALUE = 20;
+VALUE = 50;
 
 % Bentuk Umum untuk penggunaan format teks
 format_number = sprintf('Value is = %d\n',VALUE);
@@ -11,27 +11,30 @@ fprintf(format_number);
 A = 5;
 B = 5.25;
 C = 6.25;
-D = 1500;
+D = 3500;
+E = 4000;
 
 % Umumnya ada 6 specifier data (d,f,g,e,c,s)
-total_value = sprintf('A = %d\n, B = %f\n, C = %g\n, D = %e\n',A,B,C,D);
+total_value = sprintf('A = %d\n, B = %f\n, C = %g\n, D = %e\n, E = %d\n',A,B,C,D,E);
 fprintf(total_value);
 
 % Specifier c dan s
 % %c untuk Single Karakter
-Karakter = 'Xing Kong';
-teks_karakter = sprintf('karakter = %c \n', Karakter);
-fprintf(teks_karakter);
+Character1 = 'Strawberry Vanilla Milkshake';
+Character2 = 'Kunyit Jahe Gula Aren';
 
-teks_string = sprintf('string = %s \n', Karakter);
+teks_Character = sprintf('Character = %c \n', Character1);
+fprintf(teks_Character);
+
+teks_string = sprintf('string = %s \n', Character1);
 fprintf(teks_string);
 
-teks_string = sprintf('string = %s \n', 'New Character Unlocked');
+teks_string = sprintf('string = %s \n', Character2);
 fprintf(teks_string);
 
 % sprintf ==> print untuk formating teks/teks format
-% fprintf ==> Langsung munculkan VALUE = 20
+% fprintf ==> Langsung munculkan VALUE = 50
 % Memasukan beberapa variabel dalam 1 String
 %  %d ==> Decimal  , dalam Matlab penggunaan ini bagusnya digunakan pada file Integer jika digunakan pada file float/double kemungkinan hasilnya kurang bagus 
 %  %f ==> Fix Point
-%  %e ==> Eksponensial ==> D = 1.50000e+03    ===> e+03 artinya e pangkat 3
+%  %e ==> Eksponensial ==> D = 3.50000e+03    ===> e+03 artinya e pangkat 3
