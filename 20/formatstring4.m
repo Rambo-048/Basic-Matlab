60bf07d19bcbe73b6510523fe935d923e739e374
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-31 11:34:35 +0700

    Add files via upload

--- 20/formatstring4.m
+++ 20/formatstring4.m
@@ -1,7 +1,7 @@
 clear
 clc
 
-VALUE = 30;
+VALUE = 50;
 
 % Bentuk Umum untuk penggunaan format teks
 format_number = sprintf('Value is = %d\n',VALUE);
@@ -20,21 +20,22 @@ fprintf(total_value);
 
 % Specifier c dan s
 % %c untuk Single Karakter
-Menu1 = 'Sup Kacang Hijau';
-Menu2 = 'Cokelat Panas';
+Menu1 = 'Strawberry Vanilla Milkshake';
+Menu2 = 'Kunyit Jahe Gula Aren';
 
-% Memasukan variabel dalam String yang ditentukan
+% Memasukan variabel dalam String yang ditentukan 
 String1 = sprintf('Char -> %c \n', Menu1);
 fprintf(String1);
 
 String2 = sprintf('String 1 = %s \n', Menu1);
 fprintf(String2);
 
-String3 = sprintf('String 2 = %s \n', 'New Character Unlocked');
+String3 = sprintf('String 2 = %s \n', Menu2);
 fprintf(String3);
 
 % sprintf ==> print untuk formating teks/teks format
-% fprintf ==> Langsung munculkan VALUE = 30
+% fprintf ==> Langsung munculkan VALUE = 50
 %  %d ==> Decimal  , dalam Matlab penggunaan ini bagusnya digunakan pada file Integer jika digunakan pada file float/double kemungkinan hasilnya kurang bagus 
 %  %f ==> Fix Point
 %  %e ==> Eksponensial ==> D = 3.50000e+03    ===> e+03 artinya e pangkat 3
+
