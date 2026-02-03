43259693e06dda54a25bc0171b546cbd4b9514a2
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-02-03 12:23:27 +0700

    Update menulisData1.m

--- 25/menulisData1.m
+++ 25/menulisData1.m
@@ -2,22 +2,23 @@ clear
 clc
 
 filename = 'data.dat';
-file = fopen(filename,'w');
+FILE = fopen(filename,'w');
 
 x = linspace(-4,4,20);
 
-fprintf(file,'Ini adalah data Parabola dengan persamaan f(x) = x^2 \n');
-fprintf(file,'x y \n');
+fprintf(FILE,'Ini adalah data Parabola dengan persamaan f(x) = x^2 \n');
+fprintf(FILE,'x y \n');
 
 for i = 1:length(x)
     y = parabola(x(i));
   
-  data_text = sprintf('%5.2f %5.2f \n',x(i),y);
-  fprintf(file,data_text);
+  format_number = sprintf('%5.2f %5.2f \n',x(i),y);
+  fprintf(FILE,format_number);
 end
 
-fclose(file);
+fclose(FILE);
 
 function y = parabola(x);
   y = x^2;
 end
+
